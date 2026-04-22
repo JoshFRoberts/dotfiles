022c11fb58a22f38a18bb5425e0407f228b93fa0
C0ckme15t3r <josh@die-enten-im-park-sind.gratis>
2026-04-22 14:17:13 +0200

    second commit adding a setup script

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@
 
 DIR=$(pwd)
 
-ln -s ${DIR}/scripts ~/.local/bin/
+cp -r ${DIR}/scripts ~/.local/bin/
 ln -s ${DIR}/tmux ~/.config/tmux
 ln -s ${DIR}/neovim ~/.config/nvim
+
+echo "source ${DIR}/zsh/global_aliasrc" >> ~/.zshrc
